cf74f7e827c477acd959e50bf611eb5b94be651d
Elliottmonaghan <138760999+Elliottmonaghan@users.noreply.github.com>
2026-07-29 16:52:17 +1000

    fresh start

--- bluetti_bt_connect_lib/devices/ep800.py
+++
@@ -1,5 +0,0 @@
-from ..base_devices import BaseDeviceV2
-
-
-class EP800(BaseDeviceV2):
-    pass
--- bluetti_bt_connect_lib/enums/working_mode.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum, unique
-
-
-@unique
-class WorkingMode(Enum):
-    CUSTOM = 1
-    SELF_CONSUMPTION = 2
-    BACKUP = 4
-    TIME_OF_USE = 5
